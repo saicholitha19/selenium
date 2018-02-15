fb79917cbeb022e2cf525f16f5031bfbb14baa12
sai cholitha <sai cholitha@LAPTOP-8EKN6F31>
2018-02-15 13:47:46 -0600

    code updated

--- Git_Example/src/test/java/package1/Sampletestcase2.java
+++ Git_Example/src/test/java/package1/Sampletestcase2.java
@@ -1,18 +1,14 @@
 package package1;
 
-public class Git_sampletestcase {
+public class Sampletestcase2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println("welcome to git hub");
-		
-		System.out.println("hi cholitha");
+	System.out.println("hi welocme");	
 		
 		
 		
-	
 		
 		
 	}
