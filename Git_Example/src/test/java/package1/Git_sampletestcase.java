4f7f41abd05b804cca7dced38f0547901ab02a4d
sai cholitha <sai cholitha@LAPTOP-8EKN6F31>
2018-02-15 13:38:10 -0600

    Intialising git

---
+++ Git_Example/src/test/java/package1/Git_sampletestcase.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class Git_sampletestcase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("welcome to git hub");
+		
+		
+		
+	
+		
+		
+	}
+
+}
